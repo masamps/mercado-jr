871f404d07419e04588650bdce304110b1d80c39
Matheus Sampaio <matheuslemos98@gmail.com>
2023-11-30 22:11:38 -0300

    Insere apresentação de imagens

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,29 @@
-// Home.js
-
 import React from 'react';
+import Slider from 'react-slick';
+import hortfruit from '../img/hortfruti.jpg';
+import '../styles/home.css';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Home = () => {
+
+// Configurações do carrossel
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div>
-      <h1>Bem-vindo à Tela Home!</h1>
-      {/* Adicione o conteúdo da tela Home aqui */}
+      <Slider {...settings}>
+        <div>
+          <img src={hortfruit} alt="Imagem 1" className='imgStyle'/>
+        </div>
+        {/* Adicione mais imagens conforme necessário */}
+      </Slider>
     </div>
   );
 };
