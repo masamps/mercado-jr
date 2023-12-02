d48e6368bea880ac958aa5f251e6754c46896227
Matheus Sampaio <matheuslemos98@gmail.com>
2023-12-02 13:41:39 -0300

    feat: insere imagens e inclui seção de imagem das unidades

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Slider from 'react-slick';
 import hortfruit from '../img/hortfruti.jpg';
+import natal from '../img/natal.jpg';
+import niltonTorres from '../img/NiltonTorres.png';
+import portalEden from '../img/portalEden.png';
 import '../styles/home.css';
 
 import 'slick-carousel/slick/slick.css';
@@ -15,16 +18,37 @@ const Home = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,        
+    autoplaySpeed: 2500,
   };
   return (
-    <div>
-      <Slider {...settings}>
-        <div>
-          <img src={hortfruit} alt="Imagem 1" className='imgStyle'/>
+    <>
+      <div>
+        <Slider {...settings}>
+          <div>
+            <img src={hortfruit} alt="Imagem 1" className='imgStyle' />
+          </div>
+          <div>
+            <img src={natal} alt="Imagem 2" className='imgStyle' />
+          </div>
+          {/* Adicione mais imagens conforme necessário */}
+        </Slider>
+      </div>
+      <div className="image-div">
+        <div className="image-with-blur">
+            <img src={niltonTorres} alt="Imagem 3" />
+              <div className="image-text">
+                <p>R. Pedro José Bicudo, 62 - Jardim Nilton Torres, Sorocaba</p>
+              </div>
         </div>
-        {/* Adicione mais imagens conforme necessário */}
-      </Slider>
-    </div>
+          <div className="image-with-blur">
+            <img src={portalEden} alt="Imagem 4" />
+            <div className="image-text">
+                <p>Célio Figueiredo Silva, 263 - Portal do Éden, Itu</p>
+              </div>
+        </div>
+      </div >
+    </>
   );
 };
 
