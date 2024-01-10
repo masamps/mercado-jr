8e07f5c675e331d479c3d6a99fc31960051047a0
Matheus Sampaio <matheuslemos98@gmail.com>
2024-01-09 22:50:13 -0300

    fix: ajusta seleção barra menu

--- src/pages/navigator.js
+++ src/pages/navigator.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import '../styles/navigator.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import mercadoJRLogo from '../img/mercadoJR.png';
+import '../styles/navigator.css';
 
 function Navigator() {
+  const location = useLocation();
+
   return (
-    
     <header className="menu-header">
-        <div className="logo-container">
+      <div className="logo-container">
         <img src={mercadoJRLogo} alt="Logo" className="logo" />
       </div>
       <nav className="menu-container">
         <ul>
-          <li><Link to="/Home">Inicio</Link></li>
-          <li><Link to="/Empresa">A Empresa</Link></li>
-          <li><Link to="/Marmita">Marmita</Link></li>
-          <li><Link to="/Trabalhe">Trabalhe Conosco</Link></li>
+          <li className={location.pathname === '/Home' ? 'active' : ''}>
+            <Link to="/Home">Inicio</Link>
+          </li>
+          <li className={location.pathname === '/Empresa' ? 'active' : ''}>
+            <Link to="/Empresa">A Empresa</Link>
+          </li>
+          <li className={location.pathname === '/Marmita' ? 'active' : ''}>
+            <Link to="/Marmita">Marmita</Link>
+          </li>
+          <li className={location.pathname === '/Trabalhe' ? 'active' : ''}>
+            <Link to="/Trabalhe">Trabalhe Conosco</Link>
+          </li>
         </ul>
       </nav>
     </header>
-   
   );
 }
 
